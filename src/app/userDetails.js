27c9188833f303b3a42c61561f8833b3dce205c3
swati30v <swati30v@gmail.com>
2021-04-01 19:02:52 +0530

    fisrt

--- src/app/userDetails.js
+++ src/app/userDetails.js
@@ -8,46 +8,46 @@
         "english": "Native",
         "designation": "Sr. Developer",
         "website": "www.srdeveloper.com",
-        "decription": "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs"
+        "decription": "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print"
     },
     {
         "id": 2,
-        "name": "Vrini",
-        "address": "Indor India",
-        "experience": "3",
+        "name": "Aishwarya ",
+        "address": "Pune India",
+        "experience": "4",
         "english": "Native",
         "designation": "Sr. Developer",
         "website": "www.srdeveloper.com",
-        "decription": "this the decsriptiondsfgafs"
+        "decription": "Lorem ipsum, or lipsum as it is sometimes known,Lorem ipsum"
     },
     {
         "id": 3,
-        "name": "Sweta",
-        "address": "Indor India",
+        "name": "Shweta gupta",
+        "address": "Rewa India",
         "experience": "3",
         "english": "Native",
         "designation": "Sr. Developer",
         "website": "www.srdeveloper.com",
-        "decription": "this the decsriptiondsfgafs"
+        "decription": "Lorem ipsum, or lipsum as it is sometimes known,Lorem ipsum"
     },
     {
         "id": 4,
-        "name": "Swati",
+        "name": "Swati verma",
         "address": "Indor India",
         "experience": "3",
         "english": "Native",
         "designation": "Sr. Developer",
         "website": "www.srdeveloper.com",
-        "decription": "this the decsriptiondsfgafs"
+        "decription": "Lorem ipsum, or lipsum as it is sometimes known,Lorem ipsum, or lipsum ,"
     },
     {
         "id": 5,
-        "name": "Sita",
+        "name": "Shaifali sharma",
         "address": "Indor India",
         "experience": "3",
         "english": "Native",
         "designation": "Sr. Developer",
         "website": "www.srdeveloper.com",
-        "decription": "this the decsriptiondsfgafs"
+        "decription": "Lorem ipsum, or lipsum as it is sometimes known,Lorem ipsum, or lipsum as,"
     }
 ]
